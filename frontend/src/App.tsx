4b9e42a43f4068d16ffba691fc09a3245a6584e5
Melodie Shahrvari <mshahrvari@gmail.com>
2023-12-07 21:05:41 +0100

    new config : vite, tailwind, shadcn ui, basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Chat from './components/chat/chat';
-import Home from './components/home/home';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './Pages/Dashboard/Dashboard'
+// import { Button } from "@/components/ui/button"
 
 function App() {
+
   return (
-	<Router>
-		<Routes>
-			<Route path="/chat" element={<Chat />}/>
-			<Route path="/" element={<Home />}/>
-		</Routes>
-	</Router>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}>
+    
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+//commande pour avoir d'un coup la config: rfce
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <>
+    <div >Dashboard</div>
+    <Button className='text 3xl bg-black '>coucou</Button>
+    </>
+  )
+}
+
+export default Dashboard
