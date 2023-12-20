eddda7903504cade7a7505ee79d01eb3c54f0222
fbily42 <flamby50@gmail.com>
2023-12-20 14:14:59 +0100

    test <span> et underline

--- frontend/src/Pages/Chat/ChannelUsers.tsx
+++ frontend/src/Pages/Chat/ChannelUsers.tsx
@@ -6,6 +6,7 @@ function ChannelUsers() {
 
   return (
 	<div className='bg-gray-200 p-4 h-full overflow-y-auto'>
+		<span className='text-xl underline'>Channel Users</span>
 		{channelUsers.map((user, index) => (
 			<div key={index} className='mb-2'>
 				{user}
