4525c519a46b3c94ab0f81b2a3cc291a102e9a77
Perrine GROS <pgros@bess-f4r4s6.clusters.42paris.fr>
2024-01-11 20:20:41 +0100

    started implementing authguard

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserController {
 	constructor(private userService: UserService) {}
 
+	//TESTS du AuthGuard (supprimable)
+	/*
+	@Get()
+	@UseGuards(AuthGuard)
+	getUser(@Req() req: Request) { 
+		return ({login: req['userLogin'], id: req['userID']});
+	}
+	*/
 }
