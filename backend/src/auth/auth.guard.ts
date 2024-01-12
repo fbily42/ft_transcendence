e63e2784cb30cfa2152df4d0cb3734074a7e33d8
Zakariya HAMDOUCHI <zhamdouc@bess-f2r4s1.clusters.42paris.fr>
2024-01-12 20:18:41 +0100

    authguards ajouter et fonctionnel

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, Redirect } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -7,7 +7,7 @@ export class AuthGuard implements CanActivate {
 	constructor(private jwtService: JwtService) {}
 
 
-	canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+	/* canActivate(context: ExecutionContext): boolean | Promise<boolean> {
 		const request = context.switchToHttp().getRequest();
 		
 		//TODO: A l'avenir, les JWT seront toujours transmis dans le header['Authorization'] donc l'extraction du JWT est a modifier.
@@ -22,6 +22,51 @@ export class AuthGuard implements CanActivate {
 		// console.log(decode.sub);
 		request['userLogin'] = decode.login;
 		request['userID'] = decode.sub;
+		return false;
+	} */
+	canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const response = context.switchToHttp().getResponse();
+		
+		//TODO: A l'avenir, les JWT seront toujours transmis dans le header['Authorization'] donc l'extraction du JWT est a modifier.
+		// const cookie: string = request.headers['authorization'];
+		const cookie: string = request.headers.authorization;
+
+		//check if authorization exist, if it doesn't we redirect to  the authentification step
+		if (!cookie)
+		{
+			// console.log(cookie);
+			console.log('NO PERMISSION : authorization doesn\'t exist');
+			response.redirect('http://localhost:3000/auth/');
+			return false;
+		}
+
+		try {
+			//Get the JWT, then Verify if it's correct
+			const encodedJwt: string = cookie.split(' ', 2)[1];
+			// console.log(encodedJwt);
+			this.jwtService.verify(encodedJwt);
+			const decode = this.jwtService.decode(encodedJwt);
+			request['userLogin'] = decode.login;
+			request['userID'] = decode.sub;
+			// console.log('tout est bon')
+			// console.log(decode);
+			return true;
+		}
+		catch (error)
+		{
+			// console.error('Erreur de vérification du JWT :', error);
+			console.log('NO PERMISSION : authorization incorrect');
+			response.redirect('http://localhost:3000/auth/');
+			return false;
+		}
+		// console.log(encodedJwt);
+
+		//TODO: Implementer la verification du JWT
+		// console.log(decode.login);
+		// console.log(decode.sub);
 		return true;
 	}
+
+	
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -4,27 +4,29 @@ import { AuthGuard } from 'src/auth/auth.guard';
 import { Request } from 'express';
 
 @Controller('user')
+@UseGuards(AuthGuard)
+
 export class UserController {
 	constructor(private userService: UserService) {}
 
 	@Get('me')
-	@UseGuards(AuthGuard)
 	async getUserInfo(@Req() req: Request) {
 
 		//To access userLogin and userID in req, see example below
 		const token = req.headers.authorization;
 		const jwt = token.split(' ')
 		
+		console.log('hello');
 		return  await this.userService.getInfo(jwt[1]);
 	}
 
 	//TESTS du AuthGuard (supprimable)
-	/*
+	
 	@Get()
 	@UseGuards(AuthGuard)
 	getUser(@Req() req: Request) { 
 		return ({login: req['userLogin'], id: req['userID']});
 	}
-	*/
+	
 
 }
