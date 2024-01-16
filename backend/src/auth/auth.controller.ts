e5a3ca75082e9ae1d9b352e2534d4790664c7983
Zakariya HAMDOUCHI <zhamdouc@bess-f2r4s4.clusters.42paris.fr>
2024-01-16 19:59:54 +0100

    handle error authguards

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -9,32 +9,37 @@ export class AuthController{
 	@Get('42')
 	async login(@Req() req : Request, @Res() res : Response) {
 
-		const code = req.query.code as string;
+		try {
+			const code = req.query.code as string;
+			
+			// Get the token access from 42api
+			const token = await this.authService.getToken(code);
+			
+			// Get the login of the user from 42api
+			const login :string = await this.authService.getUserLogin(token);
+			
+			// Find if User exists, create if doesnt
+			const user = await this.authService.findUser(login, token);
+			
+			// Create JWT and add to the user in DB
+			const jwt = await this.authService.createJwt(user);
 		
-		// Get the token access from 42api
-		const token = await this.authService.getToken(code);
-		
-		// Get the login of the user from 42api
-		const login :string = await this.authService.getUserLogin(token);
-		
-		// Find if User exists, create if doesnt
-		const user = await this.authService.findUser(login, token);
-		
-		// Create JWT and add to the user in DB
-		const jwt = await this.authService.createJwt(user);
-
-		// Create cookie for browser
-		// res.cookie('jwt', jwt);
-		res.cookie('jwt', jwt, {
-			httpOnly: true,
-			sameSite: 'strict',
-			secure: true,
-			domain: process.env.FRONTEND_DOMAIN,
-		});
+			// Create cookie for browser
+			// res.cookie('jwt', jwt);
+			res.cookie('jwt', jwt, {
+				httpOnly: true,
+				sameSite: 'strict',
+				secure: true,
+				domain: process.env.FRONTEND_DOMAIN,
+			});
+			
+			res.redirect("http://localhost:3000/");
+		} catch(error) {
+			throw error;
+		}
 		// res.cookie('jwt', jwt);
 
 		// Redirect to Dashboard
-		res.redirect("http://localhost:3000/");
 	}
 }
 /* 
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
 
 @Injectable({})
 export class AuthService {
@@ -10,28 +12,49 @@ export class AuthService {
 
 	async getToken(code :string){
 
-		//console.log(` Client UID : ${process.env.CLIENT_UID}\n Client Secret : ${process.env.CLIENT_SECRET}\n Code : ${code}\n Redirect_uri : ${process.env.REDIRECT_URI}`);
-		const response = await fetch("https://api.intra.42.fr/oauth/token",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": 'application/x-www-form-urlencoded',
-			  },
-			body: `grant_type=authorization_code&client_id=${process.env.CLIENT_UID}&client_secret=${process.env.CLIENT_SECRET}&code=${code}&redirect_uri=${process.env.REDIRECT_URI}`,
-		})
+		try {
 
-		const token = await response.json();
-		return token;
+			const response = await fetch("https://api.intra.42.fr/oauth/token",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": 'application/x-www-form-urlencoded',
+				  },
+				body: `grant_type=authorization_code&client_id=${process.env.CLIENT_UID}&client_secret=${process.env.CLIENT_SECRET}&code=${code}&redirect_uri=${process.env.REDIRECT_URI}`,
+			})
+	
+			if (!response.ok){
+				throw new HttpException('Unexpected HTTP error ', HttpStatus.INTERNAL_SERVER_ERROR);//check HttpStatus
+				
+				//throw new Error(`Erreur HTTP : ${response.status} - ${response.statusText}`;)
+			}
+			const token = await response.json();
+			return token;
+		} catch(error)
+		{
+			throw new HttpException('Unexpected HTTP error ', HttpStatus.INTERNAL_SERVER_ERROR);//check HttpStatus
+		}		//console.log(` Client UID : ${process.env.CLIENT_UID}\n Client Secret : ${process.env.CLIENT_SECRET}\n Code : ${code}\n Redirect_uri : ${process.env.REDIRECT_URI}`);
 	};
 
 	async getUserLogin(token :any){
 
-		const response = await fetch("https://api.intra.42.fr/v2/me", {
-			headers: {Authorization: `Bearer ${token.access_token}`}
-		});
+		try {
+			const response = await fetch("https://api.intra.42.fr/v2/me", {
+				headers: {Authorization: `Bearer ${token.access_token}`}
+			});
+			
+			if (!response.ok)
+				throw new HttpException('Unexpected HTTP error ', HttpStatus.INTERNAL_SERVER_ERROR);//check HttpStatus
 
-		const user = await response.json();
-		return user.login;
+				
+			const user = await response.json();
+			return user.login;
+
+		} catch(error)
+		{
+			throw new HttpException('Unexpected HTTP error ', HttpStatus.INTERNAL_SERVER_ERROR);//check HttpStatus
+
+		}	
 	};
 
 	async findUser(login :string, token :any){
