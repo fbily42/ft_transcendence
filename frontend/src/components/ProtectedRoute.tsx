8cf76200bbab622a44f74b53a2a47d0eaf223481
Melodie SHAHRVARI <mshahrvari@gmail.com>
2024-01-16 19:43:24 +0100

    Solved navigate for ProtectedRoute

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,11 +1,11 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, useNavigate,Navigate } from "react-router-dom";
 import { useEffect, useState  } from "react";
 import axios from 'axios';
 
 
 function ProtectedRoute() : JSX.Element {
-
-	const [auth, setAuth] = useState<Boolean>(false);
+	const navigate = useNavigate()
+	const [auth, setAuth] = useState<boolean>(false);
 
 	useEffect(() => {
 		async function checkIsAuth() {
@@ -13,28 +13,27 @@ function ProtectedRoute() : JSX.Element {
 				const response = await axios.get("http://localhost:3333/auth/isAuth", {
 					withCredentials: true
 				});
-
-				console.log("Response", response);
 				setAuth(true);
-				console.log("Authentified 1 = ", auth);
 			}
 			catch (error){
 				console.log("Error", error);
-				setAuth(false);
-				console.log("Authentified 2 = ", auth);
+				setAuth(() => {
+					return false;
+				});
+				navigate('/auth');
 			}
 		}
 		checkIsAuth();
 	}, []);
 
-	useEffect(() => {
- 		console.log("auth2", auth);
-	}, [auth]);
-
 	console.log("auth", auth);
-	return (
-		auth ? <Outlet/> : <Navigate to="/auth"/>
-	);
+	// return (
+		// auth ? <Outlet/> : <Navigate to="/auth"/>
+	// );
+	if (auth){
+		return <Outlet></Outlet>
+	}
+	return <div></div>
 }
 
 export default ProtectedRoute;
