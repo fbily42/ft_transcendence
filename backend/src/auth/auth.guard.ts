d8525690acc89fb7dd4dad188e08e0d07612e2d5
Perrine GROS <pgros@bess-f2r4s6.clusters.42paris.fr>
2024-01-16 15:44:15 +0100

    removed redirect at the end of authguard aka. 8h de travail

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -8,25 +8,18 @@ export class AuthGuard implements CanActivate {
 
 	canActivate(context: ExecutionContext): boolean | Promise<boolean> {
 		const request = context.switchToHttp().getRequest();
-		const response = context.switchToHttp().getResponse();
 		
-		//TODO: A l'avenir, les JWT seront toujours transmis dans le header['Authorization'] donc l'extraction du JWT est a modifier.
-		// const cookie: string = request.headers['authorization'];
-		const cookie: string = request.headers.authorization;
+		const encodedJwt: string = request.cookies.jwt;
 
 		//check if authorization exist, if it doesn't we redirect to  the authentification step
-		if (!cookie)
+		if (!encodedJwt)
 		{
-			// console.log(cookie);
-			console.log('NO PERMISSION : authorization doesn\'t exist');
-			response.redirect('http://localhost:3000/auth/');
+			console.log('NO PERMISSION : jwt cookie doesn\'t exist');
 			return false;
 		}
 
 		try {
 			//Get the JWT, then Verify if it's correct
-			const encodedJwt: string = cookie.split(' ', 2)[1];
-			// console.log(encodedJwt);
 			this.jwtService.verify(encodedJwt);
 			const decode = this.jwtService.decode(encodedJwt);
 			request['userLogin'] = decode.login;
@@ -39,11 +32,9 @@ export class AuthGuard implements CanActivate {
 		{
 			// console.error('Erreur de vérification du JWT :', error);
 			console.log('NO PERMISSION : authorization incorrect');
-			response.redirect('http://localhost:3000/auth/');
 			return false;
 		}
 
-		return true;
 	}
 
 	
