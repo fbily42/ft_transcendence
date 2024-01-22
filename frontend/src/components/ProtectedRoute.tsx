718906b7b2710e13f4571f61b91ce95dc787e5b4
Zheylkoss <100791060+Zheylkoss@users.noreply.github.com>
2024-01-22 03:07:36 +0100

    add the instance of axios, to intercept fail authentification

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import { useEffect, useState  } from "react";
 import axios from 'axios';
-
+import instance from "@/axiosConfig";
 
 function ProtectedRoute() : JSX.Element {
 	const navigate = useNavigate();
