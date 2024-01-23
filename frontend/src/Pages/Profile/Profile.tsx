0be3742b29dbffc330310de43e45328157ad292c
Melodie SHAHRVARI <mshahrvari@gmail.com>
2024-01-23 20:17:14 +0100

    2FA btn added

--- frontend/src/Pages/Profile/Profile.tsx
+++ frontend/src/Pages/Profile/Profile.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import UserCards from '@/components/User/userCards/UserCards'
+import { Button } from '@/components/ui/button'
 
 function Profile(){
     return (
         <div className="flex justify-between pl-[102px] md:pl-[112px] lg:pl-[122px] pb-[36px] pr-[16px] md:pr-[26px] lg:pr-[36px] h-[90vh] gap-[16px] md:gap-[26px] lg:gap-[36px]">
             <div className="flex flex-col w-[60%] sm:w-[60%] md:w-[70%] lg:w-[80%] h-full rounded-[26px] md:rounded-[30px] lg:rounded-[36px] shadow-drop">
-                <div className="flex w-full h-[50%] justify-between bg-blue-100 rounded-t-[26px] md:rounded-t-[30px] lg:rounded-t-[36px]"></div>
+                <div className="flex w-full items-center justify-center h-[50%] bg-white rounded-t-[26px] md:rounded-t-[30px] lg:rounded-t-[36px] gap-[14px]">
+                  <Button variant={'default'}>Change Avatar</Button>
+                  <Button variant={'outlineBlue'} className=''>Setup 2FA</Button>
+                </div>
                 <div className="flex w-full h-[50%] justify-between bg-blue-200 rounded-b-[26px] md:rounded-b-[30px] lg:rounded-b-[36px]"></div>
             </div>
             <div className="flex flex-col w-[40%] sm:w-[40%] md:w-[30%] lg:w-[30%] h-full bg-white rounded-[26px] md:rounded-[30px] lg:rounded-[36px] gap-[36px] shadow-drop">
