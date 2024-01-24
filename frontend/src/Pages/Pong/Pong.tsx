c49f2994c0ac4affd2aa061978ee21bd83418fc0
Zakariya HAMDOUCHI <zhamdouc@bess-f2r4s2.clusters.42paris.fr>
2024-01-24 18:45:27 +0100

    debut game

--- frontend/src/Pages/Pong/Pong.tsx
+++ frontend/src/Pages/Pong/Pong.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Dashboard from '../Dashboard/Dashboard'
+import Chat from '../Chat/Chat';
 
 function Pong() {
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const context = canvas.getContext('2d');
+	
+		function updateGame() {
+			// Mettre à jour les positions de la raquette et de la balle
+		}
+	
+		function drawGame() {
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			// Dessiner la raquette, la balle, le score, etc.
+		}
+	
+		function gameLoop() {
+			updateGame();
+			drawGame();
+			requestAnimationFrame(gameLoop);
+		}
+	
+		gameLoop();
+	}, []);
+	
+
   return (
-    <div>Pong</div>
+    <div className='flex justify-center items-center'>
+		<Link to="/dashboard" >
+			<Button>PLAY</Button>
+		</Link>
+	</div>
   )
 }
 
