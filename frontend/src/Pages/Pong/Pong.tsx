4870f012d747df102c4305091c8fb62bcf9c268d
Zakariya HAMDOUCHI <zhamdouc@bess-f2r4s3.clusters.42paris.fr>
2024-01-26 19:02:20 +0100

    start the creation of the modal

--- frontend/src/Pages/Pong/Pong.tsx
+++ frontend/src/Pages/Pong/Pong.tsx
@@ -1,41 +1,66 @@
 import { Button } from '@/components/ui/button'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Dashboard from '../Dashboard/Dashboard'
 import Chat from '../Chat/Chat';
+import Modal from '@/components/Modal';
+import axios from 'axios';
 
+
+// il y a 2 boutons, un qui permet de chercher un amis, un autre qui permet d'affronter quelqu'un aleatoirement
 function Pong() {
-
+		
+	const [open, setOpen] = useState<boolean>(false);
+	
 	useEffect(() => {
-		const canvas = canvasRef.current;
-		const context = canvas.getContext('2d');
-	
-		function updateGame() {
-			// Mettre à jour les positions de la raquette et de la balle
-		}
-	
-		function drawGame() {
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			// Dessiner la raquette, la balle, le score, etc.
-		}
-	
-		function gameLoop() {
-			updateGame();
-			drawGame();
-			requestAnimationFrame(gameLoop);
-		}
-	
-		gameLoop();
+
+		const getChannels = async () => {
+			try {
+				
+				const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/chat/channel/all`,{
+					withCredentials: true,
+				})
+				console.log(response.data);		
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getChannels();
 	}, []);
+	// useEffect(() => {
+	// 	const canvas = canvasRef.current;
+	// 	const context = canvas.getContext('2d');
+	
+	// 	function updateGame() {
+	// 		// Mettre à jour les positions de la raquette et de la balle
+	// 	}
+	
+	// 	function drawGame() {
+	// 		context.clearRect(0, 0, canvas.width, canvas.height);
+	// 		// Dessiner la raquette, la balle, le score, etc.
+	// 	}
+	
+	// 	function gameLoop() {
+	// 		updateGame();
+	// 		drawGame();
+	// 		requestAnimationFrame(gameLoop);
+	// 	}
+	
+	// 	gameLoop();
+	// }, []);
 	
 
   return (
     <div className='flex justify-center items-center'>
+		<Modal open={open} onClose={() => setOpen(false)}>
+					<GameForm></GameForm>
+				</Modal>
 		<Link to="/dashboard" >
 			<Button>PLAY</Button>
 		</Link>
 	</div>
   )
-}
+  }
 
 export default Pong
---
+++ frontend/src/components/Pong/GameForm.tsx
@@ -0,0 +1,29 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import { useState } from "react";
+import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+  } from "@/components/ui/card"
+
+
+import React, { useRef, KeyboardEvent } from 'react';
+
+function GameForm(){
+	
+
+	return (
+		<div>
+			<h1></h1>
+		</div>
+	)
+}
+const GameForm : React.FC<GameForm> = () => {
+	
+}
