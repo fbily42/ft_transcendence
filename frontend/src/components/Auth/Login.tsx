b31947d7d1906e6ee6612aa23ca98b43ede47c85
Perrine GROS <pgros@bess-f2r1s10.clusters.42paris.fr>
2024-02-06 21:43:24 +0100

    improved login page

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,5 +1,8 @@
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
+import PinguLogo from '../../assets/welcome-assets/PinguLogo.svg'
+import PinguFamily from '../../assets/welcome-assets/PinguFamily.svg'
+
 
 type Loginprops = {
 	children : React.ReactNode
@@ -8,27 +11,31 @@ type Loginprops = {
 const Login : React.FC<Loginprops> = ({children}) => {
 	return (
 		<div className='flex h-[100vh]'>
-			<div className='flex-1 bg-blue-300 p-4'>
-				<div>
-					Image Pingu
+			<div className='flex-1 flex flex-col justify-between items-center bg-blue-300 p-[36px]'>
+				<div className='bg-red-500'>
+					<img src={PinguLogo}></img>
 				</div>
-				<div>
-					Middle text
-					<div className='w-full h-full flex justify-center items-center'>
+				<div className="flex flex-col gap-y-[36px] items-center bg-red-400">
+					<div className='flex flex-col gap-y-[10px] justify-center text-center bg-pink-400'>
+						<div>
+							<p className='text-5xl font-bold'>WELCOME TO PINGUSCENDENCE</p>
+						</div>
+						<div>
+							<p className='text-xl'>Login to visit Pingu and his family in Antarctica</p>
+						</div>
+					</div>
+					<div className='w-full h-full flex justify-center items-center bg-pink-400'>
 						<Link to={import.meta.env.VITE_REDIRECT_URI}>
-							<Button className='bg-customYellow font-bold' >Login as 42 Student</Button>
+							<Button className='bg-customYellow text-lg font-bold' >Login as 42 Student</Button>
 						</Link>
 						{children}
 					</div>
 				</div>
-				<div>
-					Footer image
-				</div>
-				<div>
-					Snow
+				<div className='bg-red-300'>
+					<img src={PinguFamily}></img>
 				</div>
 			</div>
-			<div className='hidden lg:flex lg:flex-1 bg-green-300 p-4'>
+			<div className='hidden lg:flex lg:flex-1 bg-green-300 p-[36px]'>
 				Div 2
 			</div>
 		</div>
