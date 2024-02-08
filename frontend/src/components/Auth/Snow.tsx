164fb699de75eeb714b2a89ff5a6291925922d6b
Perrine GROS <pgros@paul-f4Ar7s8.clusters.42paris.fr>
2024-02-08 21:12:22 +0100

    finished login welcome page youhou + prettiered a bunch of files

--- frontend/src/components/Auth/Snow.tsx
+++ frontend/src/components/Auth/Snow.tsx
@@ -1,43 +1,50 @@
-import { useEffect, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSnowPreset } from "@tsparticles/preset-snow";
-import { loadImageShape } from "@tsparticles/shape-image";
-import { IOptions } from "@tsparticles/engine";
-import Snowflake from "../../assets/welcome-assets/snowflake.svg"
+import { useEffect, useState } from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { loadSnowPreset } from '@tsparticles/preset-snow'
+import { loadImageShape } from '@tsparticles/shape-image'
+import { IOptions } from '@tsparticles/engine'
+import Snowflake from '../../assets/welcome-assets/snowflake.svg'
 
-const Snow : React.FC = () => {
-	const [init, setInit] = useState(false);
+const Snow: React.FC = () => {
+    const [init, setInit] = useState(false)
 
-	useEffect(() => {
-		initParticlesEngine(async (engine) => {
-		  await loadSnowPreset(engine);
-		  await loadImageShape(engine);
-		}).then(() => {
-		  setInit(true);
-		});
-	  }, []);
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSnowPreset(engine)
+            await loadImageShape(engine)
+        }).then(() => {
+            setInit(true)
+        })
+    }, [])
 
+    if (init) {
+        return (
+            <Particles
+                id="tsparticles"
+                options={{
+                    preset: 'snow',
+                    background: {
+                        opacity: 0,
+                    },
+                    particles: {
+                        opacity: {
+                            value: 1,
+                        },
+                        shape: {
+                            type: 'image',
+                            options: {
+                                image: {
+                                    src: Snowflake,
+                                },
+                            },
+                        },
+                    },
+                }}
+            />
+        )
+    }
 
-	if (init) {
-		return (<Particles id="tsparticles" options={{
-			preset: "snow",
-			background: {
-				opacity: 0
-			},
-			particles: {
-				shape: {
-					type: "image",
-					options: {
-						"image": {
-							"src": Snowflake,
-						}
-					}
-				},
-			}
-		}}/>);
-	}
-
-	return (null);
+    return null
 }
 
-export default Snow;
+export default Snow
