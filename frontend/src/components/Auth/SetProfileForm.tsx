9881cdf5bdddfecea51a14c68a56be375c7dca05
Perrine GROS <pgros@bess-f2r5s2.clusters.42paris.fr>
2024-02-11 18:31:46 +0100

    improved login set up

--- frontend/src/components/Auth/SetProfileForm.tsx
+++ frontend/src/components/Auth/SetProfileForm.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Label } from '@radix-ui/react-label'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import AvatarImg from '../User/userAvatar/AvatarImg'
+import { Photo42, Pingu } from '@/assets/avatarAssociation'
+import { useQuery } from '@tanstack/react-query'
+import { UserData } from '@/lib/Dashboard/dashboard.types'
+import { getUserMe } from '@/lib/Dashboard/dashboard.requests'
 
 type SetProfileFormprops = {
     children: React.ReactNode
 }
 
 type ProfileFormValues = {
-    name: string
+    pseudo: string
+}
+
+type ImageObject = {
+    id: number
+    imageProfile: string
+    imageBackground: string | undefined
 }
 
 const SetProfileForm: React.FC<SetProfileFormprops> = ({ children }) => {
     const { register, handleSubmit } = useForm<ProfileFormValues>()
     const [errorMessage, setErrorMessage] = useState<string>('')
+    const initialAvatar: ImageObject = Photo42()
+    const [selectedAvatar, setSelectedAvatar] =
+        useState<ImageObject>(initialAvatar)
+
+    useEffect(() => {
+        setSelectedAvatar(initialAvatar)
+    }, [initialAvatar.imageProfile])
 
     async function onSubmit(data: ProfileFormValues) {
-        setErrorMessage('data name = ' + data.name)
+        setErrorMessage('data pseudo = ' + data.pseudo)
     }
-
+    // console.log('query data = ', data)
+    // console.log('avatar src = ', selectedAvatar)
     return (
         <div>
             <div className="flex flex-col gap-y-[10px] justify-center text-center">
@@ -36,23 +55,49 @@ const SetProfileForm: React.FC<SetProfileFormprops> = ({ children }) => {
             </div>
             <div className="w-full h-full flex flex-col justify-center items-center bg-pink-400">
                 <form
-                    className="flex flex-col justify-center items-center space-y-1"
+                    className="flex flex-col justify-center items-center space-y-3"
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <div>
-                        <Label htmlFor="name" hidden>
+                        <Label htmlFor="avatar" hidden>
+                            Avatar
+                        </Label>
+                        <div
+                            className="relative bg-[#C1E2F7] w-[80px] h-[80px] border-[3px] border-customYellow rounded-full"
+                            style={{
+                                backgroundImage: `url(${selectedAvatar.imageProfile})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                            }}
+                        >
+                            <div className="absolute -bottom-1 -right-1">
+                                <Button className="bg-customYellow rounded-full w-[30px] h-[30px]">
+                                    +
+                                </Button>
+                            </div>
+                        </div>
+                        {/* <AvatarImg
+                            onSelect={(selectedImage) =>
+                                setSelectedAvatar(selectedImage)
+                            }
+                        /> */}
+                    </div>
+                    <div>
+                        <Label htmlFor="pseudo" hidden>
                             Name
                         </Label>
                         <Input
-                            id="name"
+                            id="pseudo"
                             placeholder="Noot's name"
-                            {...register('name')}
+                            {...register('pseudo')}
                         />
                     </div>
                     <div className="text-red-600 text-sm">{errorMessage}</div>
-                    <Button className="bg-customYellow text-lg font-semi-bold">
-                        I'm ready to noot!
-                    </Button>
+                    <div>
+                        <Button className="bg-customYellow text-lg font-semi-bold">
+                            I'm ready to noot!
+                        </Button>
+                    </div>
                 </form>
                 {children}
             </div>
