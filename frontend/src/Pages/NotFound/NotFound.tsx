602c2af72c5c99d01bcb0b2a8f1c49a967e287d8
Perrine GROS <pgros@bess-f2r1s2.clusters.42paris.fr>
2024-02-27 18:24:19 +0100

    passed 404 NotFound background to black

--- frontend/src/Pages/NotFound/NotFound.tsx
+++ frontend/src/Pages/NotFound/NotFound.tsx
@@ -3,11 +3,11 @@ import PinguEpisode from '../../assets/other/Pingu_EpisodeExtract.mp4'
 
 function NotFound(): JSX.Element {
     return (
-        <div className="h-[100vh] w-full">
+        <div className="h-[100vh] w-full bg-black">
             <Snow />
 
             <div className="flex flex-col h-full items-center justify-center p-[36px]">
-                <div className="flex flex-col justify-center items-center z-10">
+                <div className="flex flex-col justify-center items-center z-10 text-customBlue">
                     <p className="text-9xl font-bold">404</p>
                     <p className="text-5xl font-semibold">Not found</p>
                 </div>
