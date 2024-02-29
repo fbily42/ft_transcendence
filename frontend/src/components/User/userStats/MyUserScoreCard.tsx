d2932589d31aec335da9bb263a34ba7cac0fa2ec
Melodie SHAHRVARI <mshahrvari@gmail.com>
2024-02-29 19:22:38 +0100

    badge placed

--- frontend/src/components/User/userStats/MyUserScoreCard.tsx
+++ frontend/src/components/User/userStats/MyUserScoreCard.tsx
@@ -1,12 +1,25 @@
 import { getUserMe } from '@/lib/Dashboard/dashboard.requests'
 import { UserData } from '@/lib/Dashboard/dashboard.types'
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogDescription,
+    DialogTitle,
+} from '@/components/ui/dialog'
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import NoAchievements from '@/assets/badges/No achivement.png'
+import { Avatar } from '@/components/ui/avatar'
+import { AvatarImage } from '@radix-ui/react-avatar'
 
 export default function MyUserScoreCard() {
     const { data: me } = useQuery<UserData>({
         queryKey: ['me'],
         queryFn: getUserMe,
     })
+
+    const [open, setOpen] = useState<boolean>(false)
 
     return (
         <div
@@ -24,9 +37,28 @@ export default function MyUserScoreCard() {
                         </div>
                     </div>
                 </div>
-                <div className="border-2 border-[#C1E2F7] rounded-full">
-                    <div className="w-[71px] h-[71px]"></div>
+                <div
+                    className="flex items-center justify-center w-[70px] h-[70px] border-[#C1E2F7] rounded-full"
+                    onClick={() => setOpen(true)}
+                >
+                    <Avatar className="aspect-square w-[100px] h-fit flex items-center justify-center">
+                        <AvatarImage
+                            className="rounded-full object-cover"
+                            src={NoAchievements}
+                        ></AvatarImage>
+                    </Avatar>
                 </div>
+                <Dialog open={open} onOpenChange={setOpen}>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Achievements</DialogTitle>
+                            <DialogDescription>
+                                Choose the badge you want to display on your
+                                profile.
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
             </div>
         </div>
     )
