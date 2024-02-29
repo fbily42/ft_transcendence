098fa2b3bb1ddd3d75854c581d6cb33a623732da
Perrine GROS <pgros@paul-f4Br5s5.clusters.42paris.fr>
2024-02-29 17:55:55 +0100

    changed pseudo limit to 10char

--- backend/src/uploads/uploads.dto.ts
+++ backend/src/uploads/uploads.dto.ts
@@ -3,7 +3,7 @@ import { IsAlpha, IsOptional, IsString, MaxLength, MinLength } from "class-valid
 export class FormDto {
 	@IsString()
 	@MinLength(2, {message: 'Pseudo must be at least 2 characters long'})
-	@MaxLength(20, {message: 'Pseudo can not exceed 20 characters'})
+	@MaxLength(10, {message: 'Pseudo can not exceed 10 characters'})
 	@IsAlpha('en-US', {message: 'Pseudo can only contain letters'})
 	pseudo: string;
 
